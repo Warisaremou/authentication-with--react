0ed4f76552354d81e68be0e7fafd7fc35f042b61
waris <warisaremou2003@gmail.com>
2023-01-02 18:18:51 +0100

    Suppression de contenues inutiles

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,8 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext } from "react";
 import Login from "./Login";
-import { Home, Acceuil } from "../Pages";
-import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import { Acceuil } from "../Pages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { useLocalStorage } from "../hooks/useLocalStorage";
 import ProtectedRoute from "./ProtectedRoute";
 
 export const LoginContext = createContext();
@@ -10,7 +10,6 @@ export const LoginContext = createContext();
 function App() {
   return (
     <div className="font-poppins">
-      
       <Router>
         <Routes>
           <Route path="login" element={<Login />} />
@@ -19,22 +18,8 @@ function App() {
           </Route>
         </Routes>
       </Router>
-      
     </div>
   );
 }
 
 export default App;
-
-// function Dashboard() {
-//   return (
-//     <div>
-//       <h1>Dashboard</h1>
-
-//       {/* This element will render either <DashboardMessages> when the URL is
-//           "/messages", <DashboardTasks> at "/tasks", or null if it is "/"
-//       */}
-//       <Outlet />
-//     </div>
-//   );
-// }
