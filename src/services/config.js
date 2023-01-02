99e14937b7f7bb11ea9fd2b59891a52fdd383f4a
waris <warisaremou2003@gmail.com>
2023-01-02 18:17:37 +0100

    Ajout de REGISTER_URL

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:8055'
+const API_URL = "http://localhost:8055";
 
-export const LOGIN_URL = API_URL + "/auth/login"
+export const LOGIN_URL = API_URL + "/auth/login";
 
+export const REGISTER_URL = API_URL + "/users";
