158b0ad5a244c0cc1802a458286805d54d674ecb
waris <warisaremou2003@gmail.com>
2023-01-05 11:21:42 +0100

    Ajout de context pour récupérer les infos de l'utilisateur

--- src/Context/AuthenticationContext.jsx
+++ src/Context/AuthenticationContext.jsx
@@ -5,8 +5,19 @@ export const AuthenticationContext = createContext({});
 export const AuthenticationContextProvider = ({ children }) => {
   const [isLogging, setIsLogging] = useState(false);
 
+  return (
+    <AuthenticationContext.Provider value={[isLogging, setIsLogging]}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
 
-  return <AuthenticationContext.Provider value={[isLogging, setIsLogging]}>
-    {children}
-  </AuthenticationContext.Provider>;
+export const UserInfoContext = createContext();
+
+export const UserInfoProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState({});
+
+  return (
+    <UserInfoContext.Provider value={[userInfo, setUserInfo]}>{children}</UserInfoContext.Provider>
+  );
 };
