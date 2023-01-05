92dfabbfc664300e422c7371584116a1dd9b9280
waris <warisaremou2003@gmail.com>
2023-01-05 11:11:30 +0100

    Ajout d'url pour les articles

--- src/services/config.js
+++ src/services/config.js
@@ -3,3 +3,5 @@ const API_URL = "http://localhost:8055";
 export const LOGIN_URL = API_URL + "/auth/login";
 
 export const REGISTER_URL = API_URL + "/users";
+
+export const ITEMS_URL = API_URL + "/items";
